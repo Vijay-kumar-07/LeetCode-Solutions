e729d188bbd441e2d8188c8b63cfc0a443c5a503
Vijay-kumar-07 <vijaykumarbudatha7@gmail.com>
2022-06-02 11:31:54 +0530

    Daily Challenge

---
+++ 867. Transpose Matrix(DC).py
@@ -0,0 +1,16 @@
+#Question:
+#Given a 2D integer array matrix, return the transpose of matrix.
+
+#The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+#Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+# Solution:
+import numpy
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+arr = numpy.transpose(matrix)
+arr.tolist()
+
+#Another approach:
+list(map(list,(zip(*matrix))))
