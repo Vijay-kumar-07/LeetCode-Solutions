5e27d799a08a721552aff98a40e9b166c64773b6
Vijay Kumar <79400703+Vijay-kumar-07@users.noreply.github.com>
2022-07-26 15:39:25 +0530

    Daily Challenge

---
+++ 236. Lowest Common Ancestor of a Binary Tree(DC).py
@@ -0,0 +1,24 @@
+# Question:
+
+# Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+# According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+
+# Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+# Output: 3
+# Explanation: The LCA of nodes 5 and 1 is 3.
+
+class TreeNode:
+     def __init__(self, x):
+         self.val = x
+         self.left = None
+         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(root, p, q):
+        def ff(n):
+            if n in (None, p, q): return n
+            l, r = ff(n.left), ff(n.right)
+            if l and r: return n
+            else: return l or r
+        return ff(root)
