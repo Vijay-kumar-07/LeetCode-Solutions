65af37bf594c3de0c567c57782ee3f74db4f6b68
Vijay Kumar <79400703+Vijay-kumar-07@users.noreply.github.com>
2022-07-27 09:02:58 +0530

    Daily Challenge

---
+++ 114. Flatten Binary Tree to Linked List(DC).py
@@ -0,0 +1,33 @@
+# Question:
+
+# Given the root of a binary tree, flatten the tree into a "linked list":
+
+# The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+# The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+# Input: root = [1,2,5,3,4,null,6]
+# Output: [1,null,2,null,3,null,4,null,5,null,6]
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        def dfs(root):
+            if not root:
+                return None
+            leftTail = dfs(root.left)
+            rightTail = dfs(root.right)
+            
+            if root.left:
+                leftTail.right = root.right
+                root.right = root.left
+                root.left = None
+            last = rightTail or leftTail or root
+            return last
+        dfs(root)
