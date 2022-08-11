ff792dfcb6c3f523d62088887ace6846bc32e030
Vijay Kumar <79400703+Vijay-kumar-07@users.noreply.github.com>
2022-08-11 10:43:53 +0530

    Daily Challenge

---
+++ 98. Validate Binary Search Tree(DC).py
@@ -0,0 +1,28 @@
+# Question:
+
+# Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+# A valid BST is defined as follows:
+
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+
+# Input: root = [2,1,3]
+# Output: true
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def helper(node, low, high):
+            if not node:
+                return True
+            if not (low < node.val < high):
+                return False
+            return helper(node.left, low, node.val) and helper(node.right, node.val, high)
+        
+        return helper(root, -inf, inf)
