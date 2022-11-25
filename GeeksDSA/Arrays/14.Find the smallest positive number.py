ec1ffcc9fd62dec1fce73a5e226eb153d037e6d2
Vijay Kumar <79400703+Vijay-kumar-07@users.noreply.github.com>
2022-11-25 10:54:56 +0530

    Update 14.Find the smallest positive number.py

--- GeeksDSA/Arrays/14.Find the smallest positive number.py
+++ GeeksDSA/Arrays/14.Find the smallest positive number.py
@@ -7,3 +7,19 @@ for i in range(1, len(arr)):
     if i not in arr:
         a.append(i)
 min(a)
+
+# November 25 2022
+def smallestmissing(arr):
+    arr.sort()
+    if max(arr) < 1:
+        return 1
+    if max(arr) == 1:
+        return 2
+    for i in range(1, max(arr)):
+        if i not in arr:
+            return i
+            break
+    
+if __name__ =='__main__':
+    arr = [2, 3, -7, 6, 8, 1, -10, 15]
+    print(smallestmissing(arr))
